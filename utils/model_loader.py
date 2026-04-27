e7e874324f858ca92711247011ff63bcc7100e88
Foram Patel <121280430+Foram1403@users.noreply.github.com>
2026-04-27 14:47:08 +0530

    Update model_loader.py

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -1,1 +1,8 @@
+import pickle
+import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+def load_model(model_name):
+    path = os.path.join(BASE_DIR, "models", model_name)
+    return pickle.load(open(path, "rb"))
